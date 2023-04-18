384e75743dc37638524af9fb9ef22e83a5c49e10
Edvaldo Batista <edvaldo1239@gmail.com>
2023-04-17 23:22:22 -0300

    primeiro commit

---
+++ js.js
@@ -0,0 +1,24 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+        function AlteraMinusculas(campo) {
+  campo.value=campo.value.toLowerCase();
+}		
+function somenteNumeros(e) {
+    var charCode = e.charCode ? e.charCode : e.keyCode;
+    // charCode 8 = backspace   
+    // charCode 9 = tab
+    if (charCode != 8 && charCode != 9) {
+        // charCode 48 equivale a 0   
+        // charCode 57 equivale a 9
+        if (charCode < 48 || charCode > 57) {
+            return false;
+        }
+    }
+}
